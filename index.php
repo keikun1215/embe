5a95b5c17fc34e0cafc877f26ffa177da3fc921f
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-03-05 14:53:11 +0900

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta name="theme-color" content="<?php $_GET['color']?>">
+    <meta name="description" content="<?php $_GET['dscr']?>！">
+    <meta http-equiv="refresh" content="0;URL='<?php $_GET['url']?>'">
+    <title> <?php $_GET['title']?> </title>
+  </head>
+  <body>
+    <p> リダイレクトしています... </p>
+    <p> リダイレクトできませんか？<a href="<?php $_GET['url']?>">これ</a>を押してください。</p>
+  </body>
+</html>
