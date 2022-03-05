44787cf0f375e6319cba76150ce069b864e0158e
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-03-05 15:14:19 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+<?php
+echo
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="theme-color" content="<?php $_GET['color']?>">
-    <meta name="description" content="<?php $_GET['dscr']?>">
-    <meta http-equiv="refresh" content="0;URL='<?php $_GET['ural']?>'">
-    <title> <?php $_GET['ttle']?> </title>
+    <meta name="theme-color" content="$_GET['color']">
+    <meta name="description" content="$_GET['dscr']">
+    <meta http-equiv="refresh" content="0;URL='$_GET['ural']'">
+    <title> $_GET['ttle'] </title>
   </head>
   <body>
-    <p> リダイレクトしています... </p>
-    <p> リダイレクトできませんか？<a href="<?php $_GET['ural']?>">これ</a>を押してください。</p>
+  <p> リダイレクトしています... </p><p> リダイレクトできませんか？<a href=" $_GET['ural'] ">これ</a>を押してください。</p>"
   </body>
 </html>
