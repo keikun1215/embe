de057b4abdcfcae59e3cc49b4342ab93bc679634
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-03-05 15:08:11 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta name="theme-color" content="<?php $_GET['color']?>">
     <meta name="description" content="<?php $_GET['dscr']?>">
-    <meta http-equiv="refresh" content="0;URL='<?php $_GET['url']?>'">
+    <meta http-equiv="refresh" content="0;URL='<?php $_GET['ural']?>'">
     <title> <?php $_GET['ttle']?> </title>
   </head>
   <body>
